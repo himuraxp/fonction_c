1e7023f3ae3f8d0451e8e009269573286fd42d09
HimuraXp <larbi_yohann@hotmail.com>
2015-06-14 18:45:42 +0200

    add j06

---
+++ ft_print_params.c
@@ -0,0 +1,22 @@
+int ft_putchar(char c);
+
+int main(int argc, char **argv)
+{
+	int i;
+	int z;
+
+	i = 1;
+	z = 0;
+	while (i < argc)
+	{
+		while (argv[i][z])
+		{
+			ft_putchar(argv[i][z]);
+			z++;
+		}
+		ft_putchar('\n');
+		i++;
+		z = 0;
+	}
+	return (0);
+}
---
+++ ft_print_program_name.c
@@ -0,0 +1,16 @@
+int ft_putchar(char c);
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	argc = 0;
+	while (argv[0][i])
+	{
+		ft_putchar(argv[0][i]);
+		i++;
+	}
+	ft_putchar('\n');
+	return (0);
+}
